f9c7eff9b12ef7f9ccb80a099405b7ff40d8b8a7
dj-jayu <73705447+dj-jayu@users.noreply.github.com>
2025-09-15 02:42:09 -0300

    added std::move semantics on GSError, GSFileError, GSDataError, GSAllocError classes constructors

--- gs_patterns.h
+++ gs_patterns.h
@@ -4,6 +4,7 @@
 #include <exception>
 #include <string>
 #include <cstring>
+#include <utility>
 #include <vector>
 #include <cstdint>
 
@@ -47,7 +48,7 @@ namespace gs_patterns
     class GSError : public std::exception
     {
     public:
-        explicit GSError (const std::string & reason) : _reason(reason) { }
+        explicit GSError (std::string  reason) : _reason(std::move(reason)) { }
         ~GSError() {}
 
         const char * what() const noexcept override { return _reason.c_str(); }
@@ -58,21 +59,21 @@ namespace gs_patterns
     class GSFileError : public GSError
     {
     public:
-        explicit GSFileError (const std::string & reason) : GSError(reason) { }
+        explicit GSFileError (std::string reason) : GSError(std::move(reason)) { }
         ~GSFileError() {}
     };
 
     class GSDataError : public GSError
     {
     public:
-        explicit GSDataError (const std::string & reason) : GSError(reason) { }
+        explicit GSDataError (std::string reason) : GSError(std::move(reason)) { }
         ~GSDataError() {}
     };
 
     class GSAllocError : public GSError
     {
     public:
-        explicit GSAllocError (const std::string & reason) : GSError(reason) { }
+        explicit GSAllocError (std::string reason) : GSError(std::move(reason)) { }
         ~GSAllocError() {}
     };
 
