7a61973c5446e17063c7c0a306081aead82ec288
dj-jayu <73705447+dj-jayu@users.noreply.github.com>
2025-09-15 00:12:36 -0300

    add explicit keyword before single argument method constructors in gs_pattern.h

--- gs_patterns.h
+++ gs_patterns.h
@@ -46,7 +46,7 @@ namespace gs_patterns
     class GSError : public std::exception
     {
     public:
-        GSError (const std::string & reason) : _reason(reason) { }
+        explicit GSError (const std::string & reason) : _reason(reason) { }
         ~GSError() {}
 
         const char * what() const noexcept override { return _reason.c_str(); }
@@ -57,21 +57,21 @@ namespace gs_patterns
     class GSFileError : public GSError
     {
     public:
-        GSFileError (const std::string & reason) : GSError(reason) { }
+        explicit GSFileError (const std::string & reason) : GSError(reason) { }
         ~GSFileError() {}
     };
 
     class GSDataError : public GSError
     {
     public:
-        GSDataError (const std::string & reason) : GSError(reason) { }
+        explicit GSDataError (const std::string & reason) : GSError(reason) { }
         ~GSDataError() {}
     };
 
     class GSAllocError : public GSError
     {
     public:
-        GSAllocError (const std::string & reason) : GSError(reason) { }
+        explicit GSAllocError (const std::string & reason) : GSError(reason) { }
         ~GSAllocError() {}
     };
 
@@ -110,7 +110,7 @@ namespace gs_patterns
     class Metrics
     {
     public:
-        Metrics(mem_access_type mType) : _mType(mType), _pattern_sizes(NTOP)
+        explicit Metrics(mem_access_type mType) : _mType(mType), _pattern_sizes(NTOP)
         {
             try
             {
@@ -197,7 +197,7 @@ namespace gs_patterns
     class InstrInfo
     {
     public:
-        InstrInfo(mem_access_type mType) : _mType(mType) { }
+        explicit InstrInfo(mem_access_type mType) : _mType(mType) { }
         ~InstrInfo() {
             delete [] iaddrs;
             delete [] icnt;
